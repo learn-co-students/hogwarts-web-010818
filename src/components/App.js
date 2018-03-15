50e61d1a97d3014ac9ab9be332c61c1178437ea1
feihafferkamp <fei.hafferkamp@gmail.com>
2018-03-15 18:21:04 -0400

    done with required deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
+import PigContainer from './PigContainer'
 
 class App extends Component {
+  state = { filterKeyword: '' }
+
+  filter = (e) => {
+    this.setState({ filterKeyword: e.target.value })
+  }
+
   render() {
     return (
       <div className="App">
-          < Nav />
-
+        < Nav filter={this.filter} />
+        < PigContainer filterKeyword={this.state.filterKeyword}/>
       </div>
     )
   }
---
+++ src/components/PigContainer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PigCard from './PigCard'
+import uuid from 'uuid';
+import hogs from '../porkers_data';
+
+
+export default class PigContainer extends React.Component {
+  state = { pigs: [...hogs] }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.filterKeyword !== nextProps.filterKeyword) {
+      this.setState({ pigs: this.applyFilter(nextProps.filterKeyword) });
+    }
+  }
+
+  applyFilter = (filterKeyword) => {
+    let allPigs = [...hogs];
+    if (filterKeyword === 'greased') {
+      return allPigs.filter((pig) => ( pig.greased ))
+    } else if (filterKeyword === 'weight') {
+      return allPigs.sort((a, b) => ( a.weight - b.weight ))
+    } else if (filterKeyword === 'name') {
+      return allPigs.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        } else if (a.name < b.name) {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+    } else {
+      return allPigs;
+    }
+  }
+
+  generatePigs = () => {
+    return this.state.pigs.map((pig) => {
+      return < PigCard info={ pig } key={ uuid() } />
+    })
+  }
+
+  render() {
+    return (
+      <div className="ui grid container">
+        <div>{ this.generatePigs() }</div>
+      </div>
+    )
+  }
+}
