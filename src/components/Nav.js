5b8a5c5e0ba4f25f6ac6c7ce7744a92dc2f4ac2f
github username <github email address>
2018-03-15 12:01:31 -0400

    first-commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,36 @@
 import piggy from '../porco.png';
 import React from 'react';
 
-const Nav = () => {
-  return (
-    <div className="navWrapper">
-      <span className="headerText">Hogwarts</span>
-      <div className="TwirlyPig">
-        <img src={piggy} className="App-logo" alt="piggy" />
+class Nav extends React.Component {
+  state = {
+    sortValue: 'name'
+  }
+
+  handleChangeSort = e => {
+    this.setState({
+      sortValue: e.target.value
+    }, () => this.props.handleSort(this.state.sortValue));
+  };
+
+
+  render () {
+    return (
+      <div className="navWrapper">
+        <span className="headerText">Hogwarts</span>
+        <div className="TwirlyPig">
+          <img src={piggy} className="App-logo" alt="piggy" />
+        </div>
+        <span className="normalText">A React App for County Fair Hog Fans</span>
+        <select className="ui dropdown"
+                onChange={this.handleChangeSort}
+                value={this.state.sortValue}>
+          <option value="name">Name</option>
+          <option value="weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water">Weight Ratio</option>
+          <option value="greased">Greased</option>
+        </select>
       </div>
-      <span className="normalText">A React App for County Fair Hog Fans</span>
-    </div>
-  )
+    )
+  }
 }
 
 export default Nav
